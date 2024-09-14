a5be9bb4f15284cead8b1082bb440557cdfc60cb
yurispeondivision <yurispeondivision@proton.me>
2024-09-14 01:20:13 -0700

    Tweak Mage fbx; Mage animator state machine wip; entity anim manager wip

---
+++ Assets/Developers/yurispeondivision/AnimConstants.cs
@@ -0,0 +1,23 @@
+namespace UndeadAssault
+{
+    static class EntityAnimParameters
+    {
+        // floats
+
+        public const string X = "X";
+        public const string Y = "Y";
+
+        // triggers
+
+        public const string FirePrimaryAttack = "FirePrimaryAttack";
+        public const string FireSecondaryAttack = "FireSecondaryAttack";
+        public const string FinishSecondaryAttack = "FinishSecondaryAttack";
+        public const string PlayDeath = "PlayDeath";
+    }
+
+    static class EntityAnimLayers
+    {
+        public const int Base = 0;
+        public const int Action = 1;
+    }
+}
