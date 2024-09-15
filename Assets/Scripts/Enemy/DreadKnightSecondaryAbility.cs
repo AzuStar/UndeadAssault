a93435832689813d1637eee6886b9319a18277a7
yurispeondivision <yurispeondivision@proton.me>
2024-09-15 12:26:34 -0700

    spin to win

--- Assets/Scripts/Enemy/DreadKnightSecondaryAbility.cs
+++ Assets/Scripts/Enemy/DreadKnightSecondaryAbility.cs
@@ -12,12 +12,14 @@ namespace UndeadAssault
         public float cooldown = 2.5f;
 
         private double _cdTimeout;
+        private Entity _entity;
         private Stats _stats;
         private AiComponent _aiComponent;
 
         void Start()
         {
-            _stats = GetComponent<Entity>().stats;
+            _entity = GetComponent<Entity>();
+            _stats = _entity.stats;
             _aiComponent = GetComponent<AiComponent>();
         }
 
@@ -40,6 +42,7 @@ namespace UndeadAssault
         public void PerformSpin()
         {
             _aiComponent.animationPaused = true;
+            _entity._animManager.FireSecondaryAttack();
             _casting = true;
             RangeIndicator indicator = Instantiate(
                 rangeIndicator,
@@ -66,6 +69,7 @@ namespace UndeadAssault
                     _cdTimeout += cooldownFormula;
                     _casting = false;
                     _aiComponent.animationPaused = false;
+                    _entity._animManager.FinishSecondaryAttack();
                 }
             );
         }
